1af42f140c914081f560530034b70f967c210cec
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-10 02:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const input = document.querySelector('input');
-
-function addingEventListener() {
-  input.addEventListener('click', function(event) {
-    return "clicked"
-  });
-}
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
