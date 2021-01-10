757881d2e03b069856380007bdf00606df75f2b8
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-10 02:42:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
+const input = document.querySelector('input');
+
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
+}
